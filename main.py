d69b04f34f7b7ba6e62994d2f37e2660599b77f8
Kazuki Shin <kazukis2@illinois.edu>
2023-02-19 00:40:41 -0600

    most updated main demo script

--- main.py
+++ main.py
@@ -98,18 +98,17 @@ def process_eye_gaze(frame, image_width, image_height, bounding_boxes, masks, in
 
 # main function
 if __name__ == '__main__':
+
     rospy.init_node("eyegaze_demo")
     rospy.loginfo("eyegaze_demo node started")
 
     stand = DualArmCommandInterface()
 
     stand.close_gripper(stand.gripper_group1_2)
-
+    
     for cnt in range(30):
         stand.move_arm_to_named_pose("eye_gaze_init", stand.arm_group1_2)
-        cam = cv2.VideoCapture(2)
-        cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-        cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        
 
         # move the image to the second screen and full screen it
         cv2.namedWindow("Image_Input", cv2.WINDOW_NORMAL)
@@ -117,12 +116,18 @@ if __name__ == '__main__':
         cv2.setWindowProperty("Image_Input", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 
+        cam = cv2.VideoCapture(2)
+        cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        # while not cam.isOpened():
+        #     print("wait for image")
         # Capture the image now (since it's a static scene)
         ret, frame = cam.read()
         img_name = "captured.png"
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
-        # cam.release()
+
+
 
         # Detect item in image
         detector = Detector(model_type="IS")
@@ -245,9 +250,10 @@ if __name__ == '__main__':
 
 
         collected_data = {}
-        data_dir = "data"
+        data_dir = "data/jooyoung/case2"
 
         loc_idx = int(msg)
+        # loc_idx = 1
 
         prepoke_joint_values = [[0.07291303631227386, 0.5727112217412902, 0.05970706001502002, -1.5218245377004473, -1.5162562064304979, 2.5258994365026552], # left robot1 #1
                                 [0.08566892725123143, 0.7206821449013185, 0.4703561566852317, -1.5080306788997362, -1.5532083902621618, 1.9684195776976718], # left robot1 #2
@@ -266,24 +272,43 @@ if __name__ == '__main__':
 
         idx = loc_idx
         if idx == -1:
-            break
+            continue
         
         if idx < 3:
             stand.move_left_arm_to_joint_values(prepoke_joint_values[idx], 2)
-            rospy.sleep(0.5)
+            rospy.sleep(1.0)
+            # ret, frame = cam.read()
+            # cv2.imshow("Image_Input", frame)
             stand.move_left_arm_to_joint_values(poke_joint_values[idx], 1)
-            rospy.sleep(0.5)
-            stand.move_left_arm_to_joint_values(prepoke_joint_values[idx], 1)
+            rospy.sleep(1.0)
+            # ret, frame = cam.read()
+            # cv2.imshow("Image_Input", frame)
+            stand.move_left_arm_to_joint_values(prepoke_joint_values[idx], 2)
         else:
             stand.move_right_arm_to_joint_values(prepoke_joint_values[idx], 2)
-            rospy.sleep(0.5)
+            rospy.sleep(1.0)
+
+            # ret, frame = cam.read()
+            # cv2.imshow("Image_Input", frame)
             stand.move_right_arm_to_joint_values(poke_joint_values[idx], 1)
-            rospy.sleep(0.5)
-            stand.move_right_arm_to_joint_values(prepoke_joint_values[idx], 1)
+            rospy.sleep(1.0)
+
+            # ret, frame = cam.read()
+            # cv2.imshow("Image_Input", frame)
+            stand.move_right_arm_to_joint_values(prepoke_joint_values[idx], 2)
+        
+        rospy.sleep(2.0)
+        ret, frame = cam.read()
+        cv2.imshow("Image_Input", frame)
         stand.move_arm_to_named_pose("eye_gaze_init", stand.arm_group1_2)
         cv2.namedWindow("Image_Input")
         cv2.setMouseCallback("Image_Input", get_click_coords)
 
+
+        ret, frame = cam.read()
+        cv2.imshow("Image_Input", frame)
+
+        cam.release()
         # Click once then press a key to exit the image (desired object)
         cv2.imshow("Image_Input", output_image)
         cv2.waitKey(0)
@@ -291,18 +316,21 @@ if __name__ == '__main__':
         # Save object class and bounding box
         collected_data["actual_object"] = {"instance idx" : instance_idx, "class": class_names[instance_idx], "bounding_box": list(output_boxes[instance_idx])}    
         collected_data["both predicted_object"] = {"instance idx" : np.argmax(final_weights), "class": class_names[np.argmax(final_weights)], "bounding_box": list(output_boxes[np.argmax(final_weights)])}
-        if np.sum(instance_eye_weights) != 0:
-            collected_data["eye-gaze predicted_object"] = {"instance idx" : np.argmax(norm_eye_weights), "class": class_names[np.argmax(norm_eye_weights)], "bounding_box": list(output_boxes[np.argmax(norm_eye_weights)])}
-        if np.sum(instance_speech_weights) != 0:
-            collected_data["speech predicted_object"] = {"instance idx" : np.argmax(norm_speech_weights), "class": class_names[np.argmax(norm_speech_weights)], "bounding_box": list(output_boxes[np.argmax(norm_speech_weights)])}
         # Save the timing of the first word and the
         collected_data["start_timing"] = {"eye-gaze": eye_gaze_start_time, "word": speech_start_time, "total_time": inference_time}
         # Save word timings as a list of lists in the format of [word, start time] for each recorded word
-        collected_data["word_timings"] = [[transcription[i], word_offsets[transcription[i]][i]["start_time"]] for i in range(len(transcription))]
+        if transcription is not None and word_offsets is not None:
+            collected_data["word_timings"] = [[transcription[i], word_offsets[transcription[i]][i]["start_time"]] for i in range(len(transcription))]
         # Save segmentation outputs as a list of lists in the format of [instance_idx, class, bounding box corner 1, bounding box corner 2] for each detected object
         collected_data["segmentation_outputs"] = [[i, class_names[i], list(output_boxes[i][:2]), list(output_boxes[i][2:])] for i in range(len(class_names))]
         # Save fixation eye gaze data as a list of lists in the format of [gaze x location, gaze y location, gaze time since system initialization] for each recorded gaze
         collected_data["eye_gaze_data"] = [[int(gaze.FPOGX[i] * detector.image_width), int(gaze.FPOGY[i] * detector.image_height), gaze.REC_Time[i]] for i in range(len(gaze.FPOGX))]
+        # Prediction using eye-gaze vs speech only
+        if np.sum(instance_eye_weights) != 0:
+            collected_data["eye-gaze predicted_object"] = {"instance idx" : np.argmax(norm_eye_weights), "class": class_names[np.argmax(norm_eye_weights)], "bounding_box": list(output_boxes[np.argmax(norm_eye_weights)])}
+        if np.sum(instance_speech_weights) != 0:
+            collected_data["speech predicted_object"] = {"instance idx" : np.argmax(norm_speech_weights), "class": class_names[np.argmax(norm_speech_weights)], "bounding_box": list(output_boxes[np.argmax(norm_speech_weights)])}
+        
         # Create "data" folder if it does not exist
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
@@ -316,13 +344,11 @@ if __name__ == '__main__':
         rospy.sleep(1)
         keyinput = input("Press Enter to continue experiment...")
         if keyinput == "q":
-            break
+            exit()
 
-    stand.move_arm_to_named_pose("eye_gaze_init", stand.arm_group1_2)
-
+    # stand.move_arm_to_named_pose("eye_gaze_init", stand.arm_group1_2)
     # rostopic echo /joint_states/position[2:8] -n 1
     # rostopic echo /joint_states/position[10:] -n 1
-
     # spin until shutdown
     
     rospy.spin()
